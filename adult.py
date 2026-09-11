81cc8d6efe987a837272f9bc3ca5637db412bb8f
rakhi-tech <dahekarrakhi@gmail.com>
2026-09-11 15:36:24 +0530

    week

--- adult.py
+++ adult.py
@@ -1,3 +1,5 @@
-age = 23
+age = int(input("Enter the age"))
 if age >= 18 :
     print("Adult : your are eligible for the drive")
+else:
+    print(" you are not eligible for the drive")
---
+++ daysodweek.py
@@ -0,0 +1,25 @@
+mon=1;
+tue=2;
+wed=3;
+thursday=4;
+fri =5;
+sat=6
+num = int(input("Enter the numer"))
+if num==1:
+    print("monday")
+elif  num==6:
+    print("tue") 
+elif  num==4:
+    print("wed")
+elif  num==45:
+        print("thurs")
+elif  num==1:
+        print("fri")
+elif  num==54:
+  print("sat")
+elif  num==67:
+    print("sat")
+else:
+     print("Not a day of weeek")
+
+    
