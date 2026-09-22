c84cde54470c0c3292d167c7be1b98fc72bc9257
rakhi-tech <dahekarrakhi@gmail.com>
2026-09-22 10:34:58 +0530

    vector

---
+++ vector.py
@@ -0,0 +1,16 @@
+class Vector2D:
+ def __init__(self,x,y):
+  self.x = x
+  self.y = y
+ def display(self):
+     print("x=",self.x)
+     print("y=",self.y)
+p1 = Vector2D(2,3)
+p2 = Vector2D(5,7)
+p1.display()
+p2.display()
+ 
+ 
+
+    
+    
